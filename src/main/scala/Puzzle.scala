fac7cb5a3ef3ad3b5a6c11b932a79660ddf150ee
Christopher Høgberg Holvik <chrish18@student.uia.no>
2021-09-08 12:43:17 +0200

    added setup from bitbucket files

--- src/main/scala/Puzzle.scala
+++ src/main/scala/Puzzle.scala
@@ -1,3 +1,10 @@
-object Puzzle {
+// Puzzle class represents the problem and also the solution
+class Puzzle(x:Int, y:Int, sol:String) { // just trivial data here
+  val sizeX=x;
+  val sizeY=y;
+  val solution=sol;
 
+  override def toString: String = {
+    s"${sizeX}x${sizeY} -->\n${solution}"
+  }
 }
--- src/main/scala/PuzzleReaderWriter.scala
+++ src/main/scala/PuzzleReaderWriter.scala
@@ -1,3 +1,37 @@
-object PuzzleReaderWriter {
+import java.io.FileWriter
+import scala.io._
 
+object PuzzleReaderWriter{
+  var unsolvedFile:String="";
+  var solvedFile:String="";
+  var lines:List[String]=Nil;
+  var fw:FileWriter=null;
+
+  def initRW(infile:String, outfile:String)={
+    unsolvedFile=infile
+    solvedFile=outfile
+    lines = Source.fromFile(unsolvedFile).getLines().toList
+    fw = new FileWriter(solvedFile, false)
+  }
+
+  def getNumPuzzles():Int={
+    val countPuzzles=lines(0).split(" ").last.toInt
+    // writing number of puzzles into solution
+    fw.write("puzzles "+countPuzzles.toString+"\n")
+    return countPuzzles
+  }
+
+  def getPuzzle(index:Int):Puzzle={
+    val sizeNumbers=lines.filter(_ startsWith("size"))(index).split(" ").last.split("x")
+    return new Puzzle(sizeNumbers(0).toInt,sizeNumbers.last.toInt,"")
+  }
+
+  def putSolution(puzzle:Puzzle)={
+    fw.write("size "+puzzle.sizeX+"x"+puzzle.sizeY+"\n")
+    fw.write(puzzle.solution+"\n")
+  }
+
+  def closing()={
+    fw.close()
+  }
 }
