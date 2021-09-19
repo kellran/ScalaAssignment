231d43a000056b79f206886a9a2405895cbd4dba
Christopher Høgberg Holvik <chrish18@uia.no>
2021-09-19 19:31:11 +0200

    added a function to update all numbers

--- src/main/scala/Puzzle.scala
+++ src/main/scala/Puzzle.scala
@@ -4,8 +4,14 @@ class Puzzle(x:Int, y:Int, puzzleList: List[List[Char]]) {
   val sizeY=y;
   val puzzle = puzzleList
 
-  def setChar(row:Int, colum:Int, char:Char): Puzzle ={
-    val ny_liste = puzzle.updated(row, puzzle(row).updated(colum, char))
+  def setChar(row:Int, column:Int, char:Char): Puzzle ={
+    val ny_liste = puzzle.updated(row, puzzle(row).updated(column, char))
+    return new Puzzle(sizeX,sizeY,ny_liste)
+  }
+
+  def setInt(savestate:Puzzle,row:Int, column:Int, sum:Int): Puzzle ={
+    val savestatelist = savestate.puzzle
+    val ny_liste = puzzle.updated(row, puzzle(row).updated(column, (savestatelist(row)(column).toInt - sum).toChar))
     return new Puzzle(sizeX,sizeY,ny_liste)
   }
 }
