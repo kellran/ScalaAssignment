5c48168169b3a5865403dce3e186ca35d4b73660
Robin hansen <Robinh18@uia.no>
2021-09-21 17:37:55 +0200

    fixed directory issue

---
+++ ScalaAssignment/PuzzleReaderWriter.scala
@@ -0,0 +1,72 @@
+
+import PuzzleSolverFunctions.char_if_valid
+
+import java.io.FileWriter
+import scala.io._
+
+object PuzzleReaderWriter{
+  var unsolvedFile:String="";
+  var solvedFile:String="";
+  var lines:List[String]=Nil;
+  var linesArray:Array[String] = Array("")
+  var puzzlelist:List[List[Char]] = List(List())
+
+  var linesArrayOfArray:Array[Array[Char]]= Array(Array());
+  var fw:FileWriter=null;
+
+
+  def initRW(infile:String, outfile:String)={
+    unsolvedFile=infile
+    solvedFile=outfile
+    lines = Source.fromFile(unsolvedFile).getLines().toList
+    fw = new FileWriter(solvedFile, false)
+  }
+
+  def getNumPuzzles():Int={
+    val countPuzzles=lines(0).split(" ").last.toInt
+    // writing number of puzzles into solution
+    fw.write("puzzles "+countPuzzles.toString+"\n")
+    return countPuzzles
+  }
+
+  def getPuzzle(index:Int):Puzzle={
+    val sizeNumbers=lines.filter(_ startsWith("size"))(index).split(" ").last.split("x")
+    return new Puzzle(sizeNumbers(0).toInt,sizeNumbers.last.toInt,List(List()))
+  }
+
+  def unsolvedToList(): Unit ={
+    puzzlelist = lines
+      .map(x => x.toList)
+      .drop(2)
+  }
+
+  //Finishes the solution, it should work now
+  def solutionFinisher(savedstate:Puzzle,pos:List[(Int,Int)]): Puzzle ={
+    val x = savedstate.sizeX
+    val y = savedstate.sizeY
+    if (pos.nonEmpty) {
+      val row = pos.head._1
+      val column = pos.head._2
+
+      val temppuzzle = char_if_valid(savedstate, row, column, x, y ,'*')
+
+      return solutionFinisher(temppuzzle,pos.drop(1))
+    }
+    return savedstate
+  }
+
+  def putSolution(puzzle:Puzzle) ={
+    fw.write("size "+puzzle.sizeX+"x"+puzzle.sizeY+"\n")
+    val templist = puzzle.puzzle
+    templist.foreach(templist => fw.write(templist.toString()
+      .replace("List","")
+      .replace("(","")
+      .replace(")","")
+      .replace(" ","")
+      .replace(",","") + "\n"))
+  }
+
+  def closing()={
+    fw.close()
+  }
+}
---
+++ ScalaAssignment/PuzzleSolver.scala
@@ -0,0 +1,83 @@
+import PuzzleReaderWriter.{closing, getNumPuzzles, getPuzzle, initRW, putSolution, puzzlelist, solutionFinisher, unsolvedToList}
+import PuzzleSolverFunctions.{all_number_pos, count_until_char, find_implicit_landlocked, find_landlocked, find_pos_of_char, greybox, light, lights, place_implicit, place_landlocked, sum_of_char, update_numbers}
+
+object PuzzleSolver extends App{
+
+  def solveCustom(puzzle:Puzzle):Puzzle = {
+    // Puzzle String to List() and start class:
+    unsolvedToList()
+    val start_puzzle = new Puzzle(puzzle.sizeX, puzzle.sizeY, puzzlelist)
+
+    println("Start:")
+    start_puzzle.puzzle.foreach(x => println(x))
+
+    // Find all zeros, and add greyboxes.
+    val zeros = find_pos_of_char(start_puzzle,List(),0,0,'0')
+    val greybox_puzzle = greybox(start_puzzle,zeros)
+    println("Greybox:")
+    greybox_puzzle.puzzle.foreach(x => println(x))
+
+    // Find all landlocked tiles and place lamps
+    val landlocked_tiles = find_landlocked(greybox_puzzle,List(),0,0)
+    val landlocked_puzzle = place_landlocked(greybox_puzzle,landlocked_tiles)
+    println("Landlocked:")
+    landlocked_puzzle.puzzle.foreach(x => println(x))
+
+    // Find all numbers and update numbers
+    val numbers = all_number_pos(landlocked_puzzle)
+    val numbers_puzzle = update_numbers(landlocked_puzzle,start_puzzle,numbers)
+    println("Numbers update:")
+    numbers_puzzle.puzzle.foreach(x => println(x))
+
+    //Re-run greybox
+    val zeros2 = find_pos_of_char(numbers_puzzle,List(),0,0,'0')
+    val greybox_puzzle2 = greybox(numbers_puzzle,zeros2)
+    println("Greybox:")
+    greybox_puzzle2.puzzle.foreach(x => println(x))
+
+
+    // Find all implicit landlocked tiles
+    // and place lamps adjecent
+    val numbers2 = all_number_pos(greybox_puzzle2)
+    val impl_land = find_implicit_landlocked(greybox_puzzle2,numbers2,List())
+    val impl_land_puzzle = place_implicit(greybox_puzzle2,impl_land)
+    println(numbers2 + " " + impl_land)
+    println("Implicit landlocked:")
+    impl_land_puzzle.puzzle.foreach(x => println(x))
+
+    // Find all numbers and update numbers
+    val numbers3 = all_number_pos(impl_land_puzzle)
+    val numbers_puzzle2 = update_numbers(impl_land_puzzle,start_puzzle,numbers3)
+    println("Numbers update:")
+    numbers_puzzle2.puzzle.foreach(x => println(x))
+
+    // Re-run greybox
+    val zeros3 = find_pos_of_char(numbers_puzzle2,List(),0,0,'0')
+    val greybox_puzzle3 = greybox(numbers_puzzle2,zeros3)
+    println("Greybox:")
+    greybox_puzzle3.puzzle.foreach(x => println(x))
+
+
+    // Cast light from lamps
+    val lamps = find_pos_of_char(greybox_puzzle3,List(),0,0,'*')
+    val light_puzzle = lights(greybox_puzzle3, lamps)
+    println("Light:")
+    light_puzzle.puzzle.foreach(x => println(x))
+
+    val solutionlamps = find_pos_of_char(light_puzzle,List(),0,0,'*')
+    val solution = solutionFinisher(start_puzzle,solutionlamps)
+
+    solution
+  }
+
+  initRW("puzzle_unsolved.txt","puzzle_solved.txt")
+  val numPuzzles=getNumPuzzles()
+
+
+  for (count<- 0 until numPuzzles) {
+    println("Solving puzzle #"+(count+1).toString)
+    putSolution(solveCustom(getPuzzle(count)))
+  }
+
+  closing()
+}
