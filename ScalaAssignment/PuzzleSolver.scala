e83590d1e25f87adffc5533c361a452d5182b285
Robin hansen <Robinh18@uia.no>
2021-09-22 13:20:08 +0200

    Finished and combined white_implicit and grey_implicit

--- ScalaAssignment/PuzzleSolver.scala
+++ ScalaAssignment/PuzzleSolver.scala
@@ -1,5 +1,5 @@
-import PuzzleReaderWriter.{closing, getNumPuzzles, getPuzzle, initRW, putSolution, puzzlelist, solutionFinisher, unsolvedToList}
-import PuzzleSolverFunctions.{all_number_pos, count_char_until_black, count_until_char, find_implicit_grey, find_implicit_landlocked, find_landlocked, find_pos_of_char, greybox, light, lights, place_implicit, place_implicits, place_landlocked, sum_of_char, update_numbers}
+import PuzzleReaderWriter._
+import PuzzleSolverFunctions._
 
 object PuzzleSolver extends App{
 
@@ -66,21 +66,29 @@ object PuzzleSolver extends App{
 
     // find grey implicits and place lamps in the white tile
     val greyboxes = find_pos_of_char(light_puzzle,List(),0,0,'G')
-    val grey_implicits = find_implicit_grey(light_puzzle,greyboxes,List())
+    val grey_implicits = find_implicit_white_grey(light_puzzle,greyboxes,List(), 1)
     println(grey_implicits)
     val grey_implicit_puzzle = place_implicits(light_puzzle,grey_implicits)
     println("grey implicits:")
     grey_implicit_puzzle.puzzle.foreach(x => println(x))
 
+    // find white implicits and place lamps in the white tile
+    val whiteboxes = find_pos_of_char(grey_implicit_puzzle,List(),0,0,'_')
+    val white_implicits = find_implicit_white_grey(grey_implicit_puzzle,whiteboxes,List(), 0)
+    println(white_implicits)
+    val white_implicit_puzzle = place_implicits(grey_implicit_puzzle,white_implicits)
+    println("white implicits:")
+    white_implicit_puzzle.puzzle.foreach(x => println(x))
+
     // Re-cast light from lamps
-    val lamps2 = find_pos_of_char(grey_implicit_puzzle,List(),0,0,'*')
-    val light_puzzle2 = lights(grey_implicit_puzzle, lamps2)
+    val lamps2 = find_pos_of_char(white_implicit_puzzle,List(),0,0,'*')
+    val light_puzzle2 = lights(white_implicit_puzzle, lamps2)
     println("Light:")
     light_puzzle2.puzzle.foreach(x => println(x))
 
     // find grey implicits and place lamps in the white tile
     val greyboxes2 = find_pos_of_char(light_puzzle2,List(),0,0,'G')
-    val grey_implicits2 = find_implicit_grey(light_puzzle2,greyboxes2,List())
+    val grey_implicits2 = find_implicit_white_grey(light_puzzle2,greyboxes2,List(), 1)
     println(grey_implicits2)
     val grey_implicit_puzzle2 = place_implicits(light_puzzle2,grey_implicits2)
     println("grey implicits:")
